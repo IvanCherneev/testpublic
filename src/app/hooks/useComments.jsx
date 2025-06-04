52b6ae8c6135fae592f7156f967698a1552ca895
Ivan Cherneev <cherneev1991@gmail.com>
2025-06-04 13:02:23 +0300

    Реализован функционал отправки комментария в БД Firebase

--- src/app/hooks/useComments.jsx
+++ src/app/hooks/useComments.jsx
@@ -3,7 +3,8 @@ import PropTypes from "prop-types";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import { useAuth } from "./useAuth";
 import { nanoid } from "nanoid";
-// import { toast } from "react-toastify";
+import commentService from "../services/comment.service";
+import { toast } from "react-toastify";
 
 const CommentsContext = React.createContext();
 
@@ -14,12 +15,13 @@ export const useComments = () => {
 export const CommentsProvider = ({ children }) => {
   const { userId } = useParams();
   const { currentUser } = useAuth();
-  // const [isLoading, setLoading] = useState(true);
+  const [isLoading, setLoading] = useState(true);
   const [comments, setComments] = useState([]);
-  // const [error, setError] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     setComments(null);
+    setLoading(false);
   }, []);
 
   const createComment = async (data) => {
@@ -30,11 +32,29 @@ export const CommentsProvider = ({ children }) => {
       created_at: Date.now(),
       userId: currentUser._id,
     };
+    try {
+      const { content } = await commentService.createComment(comment);
+      console.log(content);
+    } catch (error) {
+      errorCatcher(error);
+    }
     console.log(comment);
   };
 
+  useEffect(() => {
+    if (error !== null) {
+      toast(error);
+      setError(null);
+    }
+  }, [error]);
+
+  function errorCatcher(error) {
+    const { message } = error.response.data;
+    setError(message);
+  }
+
   return (
-    <CommentsContext.Provider value={{ comments, createComment }}>
+    <CommentsContext.Provider value={{ comments, createComment, isLoading }}>
       {children}
     </CommentsContext.Provider>
   );
